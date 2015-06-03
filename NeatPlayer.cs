d26bd43c79dfb4b4ee255e71d5f0c74420752402
Michael <wheatmam@carleton.edu>
2015-06-02 19:41:46 -0500

    Changes (great commit message)

--- NeatPlayer.cs
+++ NeatPlayer.cs
@@ -4,6 +4,7 @@ using SharpNeat.Phenomes;
 
 namespace MinesweeperLib {
     public class NeatPlayer : IPlayer {
+		public IBlackBox Brain {get; set; }
 		//creates a new neat player with the specified brain
 		public NeatPlayer(IBlackBox brain) {
 			Brain = brain;
@@ -11,28 +12,32 @@ namespace MinesweeperLib {
 		
 		public Move GetMove(MinesweeperSquare[,] board) {
 			int height = board.GetLength(1);
-			int width = board.getLength(0);
+			int width = board.GetLength(0);
 			for (var i = 0; i < height; i++) {
                 for (var j = 0; j < width; j++) {
-                    Brain.InputSignalArray[i + j*height] = board[i,j];
+					int status = -1;
+					if (board[i,j].Revealed) {
+						status = board[i,j].AdjacentMines;
+					}
+                    Brain.InputSignalArray[i + j*height] = (double) status;
                 }
             }
 			Brain.Activate();
 			Move move = null;
-			double min = double.maxValue;
+			double min = double.MaxValue;
 			for (var i = 0; i < height; i++) {
                 for (var j = 0; j < width; j++) {
 					if (board[i, j].Revealed)
 						continue;
 					
 					double score = Brain.OutputSignalArray[j*height + i];
-					if (move = null) {
-						move = newMove(i, j);
-						max = score;
-					} else if (max < score) {
+					if (move == null) {
+						move = new Move(i, j);
+						min = score;
+					} else if (min > score) {
 						move.X = i;
 						move.Y = j;
-						max = score;
+						min = score;
 					}
 				}
 			}
--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 # NEATMinesweeper
+## Michael Wheatman, David Pickart, Brian Charous, and Oliver Heywood
+## Evolutionary Computation and Artificial Life Final Project
 Minesweeper implementation for testing NEAT Algorithm
 
 [This tutorial will be helpful.](http://www.nashcoding.com/2010/07/17/tutorial-evolving-neural-networks-with-sharpneat-2-part-1/)
 
-To run on OSX:
+To run on OS X:
 
 - [Get Mono](http://www.mono-project.com/download/)
-- [Acquire SharpNeat.](http://sharpneat.sourceforge.net//) Copy the ShapNeatLib.dll from `bin` into the project directory.
-- Compile: `mcs -r:SharpNeatLib.dll *.cs`
+- Compile: `mcs -r:SharpNeatLib.dll -r:SharpNeatDomains.dll *.cs`
 - Run .exe: `mono Minesweeper.exe`
